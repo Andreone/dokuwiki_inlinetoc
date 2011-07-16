c17c6cc6c1df0f7993fbe94e31d2508d577b46eb
Andreone <nfauvet15@gmail.com>
2011-07-16 16:21:05 +0200

    reformat code

--- action.php
+++ action.php
@@ -13,20 +13,23 @@ require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_inlinetoc extends DokuWiki_Action_Plugin {
 
-   /**
-     * Register event handlers
-     */
-    function register(&$controller) {
-        $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, 'handle_renderer_content_postprocess', array());
-    }
+ /**
+   * Register event handlers
+   */
+  function register(&$controller) {
+    $controller->register_hook('RENDERER_CONTENT_POSTPROCESS', 'AFTER', $this, 'handle_renderer_content_postprocess', array());
+    $controller->register_hook('TPL_METAHEADER_OUTPUT', 'BEFORE', $this, 'handle_metaheader_output', array());
+  }
 
-    /**
-     * Replace our placeholder with the toc content
-     */
-    function handle_renderer_content_postprocess(&$event, $param) {
-        global $TOC;
-        if ($TOC) {
-            $event->data[1] = str_replace('<!-- INLINETOCPLACEHOLDER -->', '<div class="inlinetoc2">'.html_buildlist($TOC,'toc','html_list_toc').'</div>', $event->data[1]);
-        }
+  /**
+   * Replace our placeholder with the toc content
+   */
+  function handle_renderer_content_postprocess(&$event, $param) {
+    global $TOC;
+    if ($TOC) {
+      $event->data[1] = str_replace('<!-- INLINETOCPLACEHOLDER -->',
+                                    '<div class="inlinetoc2">' . html_buildlist($TOC, 'toc', 'html_list_toc') . '</div>',
+                                    $event->data[1]);
     }
+  }
 }
