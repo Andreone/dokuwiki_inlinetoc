c2f7a286c90abe695a642c672d85dbcdc607427e
Andreone <nfauvet15@gmail.com>
2013-07-27 19:26:52 +0200

    don't need to check for old toc class name

--- inlinetoc.js
+++ inlinetoc.js
@@ -3,7 +3,7 @@ function hideDokuwikiToc() {
     if(document.getElementById('inlinetoc2')) {
         var elements = document.getElementsByTagName('div');
         for(var i=0; i < elements.length; i++) {
-            if(elements[i].id == 'dw__toc' || elements[i].id == 'toc') {
+            if(elements[i].id == 'dw__toc') {
                 elements[i].style.display = 'none';
                 break;
             }
